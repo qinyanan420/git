e767984b3c2ec51517f463ccad216cc06dd2fd10
qinyanan <17732603441@163.com>
2020-11-04 13:01:54 +0800

    ‘sql’

--- sql.py
+++ sql.py
@@ -1,18 +1,12 @@
 import pymysql
-import random
-NUM = 0
 db = pymysql.connect(host='192.168.1.123', user='root', passwd='bigfish', db='newbingo', port=3306, charset='utf8')
 cursor = db.cursor()
 
-# for i in range(1000):
 # sql = 'select * from bingo_mail'
 uid = 3187
-
-for i in range(2):
+for i in range(100):
     sql ='INSERT INTO bingo_mail(id, userid, mailid, state) VALUES ("%d", "%d","%d","%d")'%(uid+i,1083924,314,0)
     cursor.execute(sql)
     db.commit()
-# res = cursor.fetchone()
-# print(res)
 cursor.close()
 db.close()
